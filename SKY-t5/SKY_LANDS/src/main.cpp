4a321000caccda0a7109959675b70726c9f0ab53
Flysteal <flytealmc@gmail.com>
2025-01-03 22:51:47 +0200

    3D with movment im ok..

--- SKY-t5/SKY_LANDS/src/main.cpp
+++ SKY-t5/SKY_LANDS/src/main.cpp
@@ -11,6 +11,7 @@
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
+#include "Camera.h"
 
 const unsigned int width = 800;
 const unsigned int height = 800;
@@ -80,55 +81,30 @@ int main()
     VBO1.Unbind();
     EBO1.Unbind();
 
-    GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
-    // this is for shader float value
 
     //texture
     Texture popCat("/media/fly/o-o/SKY-t5/SKY_LANDS/Resourcefiles/Textures/meme.png",
     GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
     popCat.texUnit(shaderProgram, "tex0", 0);
 
-    float rotation = 0.0f;
-    double prevTime = glfwGetTime();
-
-
-    //glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSwapInterval(true); //vsync on 
-    glViewport(0, 0, height, height); 
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);// what is closer then others
+
+
+    Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
 
     while (!glfwWindowShouldClose(window))
     {
 
     glClearColor(0.02f, 0.13f, 0.23f, 1.0f); //color
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clears buffer
 
     shaderProgram.Activate();
 
-    double crntTime = glfwGetTime();
-	if (crntTime - prevTime >= 1 / 60)
-	{
-		rotation += 0.5f;
-		prevTime = crntTime;
-	}
+    camera.Inputs(window);
+	camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
 
-    glm::mat4 model = glm::mat4(1.0f);
-    glm::mat4 view  = glm::mat4(1.0f);
-    glm::mat4 proj  = glm::mat4(1.0f);
-
-    model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-	view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
-	proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-
-    int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
-    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
-
-    glUniform1f(uniID, 0.0f); // float value foe shaders !
     popCat.Bind();
     VAO1.Bind();
 
