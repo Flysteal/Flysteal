36bbf735799c0600df15548f63dbdb034c20fe2e
Flysteal <flytealmc@gmail.com>
2025-01-03 19:05:57 +0200

    i made 3D work and pepe

--- SKY-t5/SKY_LANDS/src/main.cpp
+++ SKY-t5/SKY_LANDS/src/main.cpp
@@ -17,18 +17,21 @@ const unsigned int height = 800;
 
 GLfloat vertices[] =
 {
-  -0.5, -0.5, 0.0,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f, 
-  -0.5,  0.5, 0.0,  0.0f, 1.0f, 0.0f,  0.0f, 1.0f,
-   0.5,  0.5, 0.0,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
-   0.5, -0.5, 0.0,  1.0f, 1.0f, 1.0f,  1.0f, 0.0f,
-   
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f 
 };// x, y, z, R, G, B,
 
 GLuint indices[] =
 {
-    0, 2, 1,
-    0, 3, 2,
-
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4,
 };// .. the wey to go
 
 
@@ -85,28 +88,38 @@ int main()
     GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
     popCat.texUnit(shaderProgram, "tex0", 0);
 
-
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
 
 
     //glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSwapInterval(true); //vsync on 
     glViewport(0, 0, height, height); 
+    glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window))
     {
 
     glClearColor(0.02f, 0.13f, 0.23f, 1.0f); //color
 
-    glClear(GL_COLOR_BUFFER_BIT); // clears buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clears buffer
 
     shaderProgram.Activate();
+
+    double crntTime = glfwGetTime();
+	if (crntTime - prevTime >= 1 / 60)
+	{
+		rotation += 0.5f;
+		prevTime = crntTime;
+	}
 
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view  = glm::mat4(1.0f);
     glm::mat4 proj  = glm::mat4(1.0f);
 
-    view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
-    proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+    model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+	view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+	proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
 
     int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
 	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
@@ -119,7 +132,7 @@ int main()
     popCat.Bind();
     VAO1.Bind();
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //yup
+    glDrawElements(GL_TRIANGLES, sizeof(indices)/ sizeof(int), GL_UNSIGNED_INT, 0); //yup
 
     glfwSwapBuffers(window);
     glfwPollEvents();
